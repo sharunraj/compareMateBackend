de786c1c2855721e139207caeb536327701bfa3b
Sharun Raj Nambayil <sharunraj39@gmail.com>
2023-06-25 15:07:38 +0530

    b

--- models.py
+++ models.py
@@ -11,3 +11,11 @@ class User(db.Model):
     id = db.Column(db.String(6), primary_key = True, unique = True, default=get_uuid)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.Text, nullable = False)
+    
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.String(20), nullable=False)
+    image_url = db.Column(db.String(200))
+    user_id = db.Column(db.String(6), db.ForeignKey('users.id'), nullable=False)
